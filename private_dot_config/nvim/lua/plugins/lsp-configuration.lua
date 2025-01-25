d27db29bc830ddab9072d89042144c712e6269a7
Thoogs <thoogs@pm.me>
2025-01-25 20:11:14 +0200

    Initial commit
    Contains configurations for the following:
    * hyprland
    * nvim
    * kitty
    * starship

---
+++ private_dot_config/nvim/lua/plugins/lsp-configuration.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- enable lua LSP
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      -- enable ruff formatter and lsp
+      lspconfig.ruff.setup({
+        capabilities = capabilities
+      })
+      -- enable gopls
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+    end,
+  },
+}
---
+++ private_dot_config/nvim/lua/vim-options.lua
@@ -0,0 +1,6 @@
+-- Set up tabs and leader
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
